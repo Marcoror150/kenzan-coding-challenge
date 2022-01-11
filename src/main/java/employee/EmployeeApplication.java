51e1bdc0b8ab83a60954812c7c09b13c5969f9ff
Marc Christensen <mcjoseph48@gmail.com>
2022-01-10 21:36:08 -0700

    Documentation.

--- src/main/java/employee/EmployeeApplication.java
+++ src/main/java/employee/EmployeeApplication.java
@@ -3,9 +3,17 @@ package employee;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
+/**
+ * This class represents and starts the REST employee application.
+ */
 @SpringBootApplication
-public class EmployeeApplication {
-
+public class EmployeeApplication
+{
+    /**
+     * Main method to start the Spring application.
+     *
+     * @param args The arguments passed from the command line or configuration.
+     */
     public static void main(String[] args)
     {
         SpringApplication.run(EmployeeApplication.class, args);
