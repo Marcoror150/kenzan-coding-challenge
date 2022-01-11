70775fee0d908a99b43df54627bbe7adcb691d2c
Marc Christensen <mcjoseph48@gmail.com>
2022-01-10 17:36:28 -0700

    Initial maven dependency and app class

---
+++ src/main/java/employee/EmployeeApplication.java
@@ -0,0 +1,13 @@
+package employee;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+    public static void main(String[] args)
+    {
+        SprintApplication.run(EmployeeApplication.class, args);
+    }
+}
